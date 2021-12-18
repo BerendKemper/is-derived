d1944fd402c05629f10e46ebb8f3aefb3b12a65e
Berend <berend_kemper@hotmail.com>
2021-12-18 21:03:24 +0100

    no tabs but spaces

--- isDerived.js
+++ isDerived.js
@@ -1,15 +1,12 @@
 "use strict";
-/**
- * Walks up the Target's prototype chain to find if it was derived from Source.
- * @param {*} Target class
- * @param {*} Source class
- */
+/**Walks up the Target's prototype chain to find if it was derived from Source.
+ * @param {*} Target class @param {*} Source class*/
 const isDerived = (Target, Source) => {
-	while (Target !== Source) {
-		Target = Object.getPrototypeOf(Target);
-		if (Target === null)
-			return false;
-	}
-	return true;
+    while (Target !== Source) {
+        Target = Object.getPrototypeOf(Target);
+        if (Target === null)
+            return false;
+    }
+    return true;
 };
 module.exports = isDerived;
